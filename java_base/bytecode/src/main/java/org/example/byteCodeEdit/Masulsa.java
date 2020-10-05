8cb4c4a9d4b25265e3778a78c913942730f45bd7
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-10-05 23:09:15 +0900

    java agent test

--- java_base/bytecode/src/main/java/org/example/byteCodeEdit/Masulsa.java
+++ java_base/bytecode/src/main/java/org/example/byteCodeEdit/Masulsa.java
@@ -2,7 +2,9 @@ package org.example.byteCodeEdit;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +13,12 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) {
+//        ClassLoader classLoader = Masulsa.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
 //        try {
-//            new ByteBuddy().redefine(Moja.class)
+//            new ByteBuddy().redefine(
+//                    typePool.describe("org.example.byteCodeEdit.Moja").resolve(),
+//                    ClassFileLocator.ForClassLoader.of(classLoader))
 //                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
 //                    .make().saveIn(new File("D:\\github\\java_study\\java_base\\bytecode\\target\\classes\\"));
 //        } catch (IOException e) {
