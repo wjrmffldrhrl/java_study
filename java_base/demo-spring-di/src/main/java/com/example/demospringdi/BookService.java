d24bae65d2761b1710674c0b818e17c028b98869
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-10-06 21:40:04 +0900

    reflection

---
+++ java_base/demo-spring-di/src/main/java/com/example/demospringdi/BookService.java
@@ -0,0 +1,10 @@
+package com.example.demospringdi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    BookRepository bookRepository;
+}
---
+++ java_base/reflection/src/main/java/org/example/Book.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Book {
+    private String a = "a";
+    private static String B = "BOOK";
+    private static final String C = "BOOK";
+
+    public String d = "d";
+
+    protected String e = "e";
+
+    public Book() {
+    }
+
+    public Book(String a, String b, String c) {
+        this.a = a;
+        this.B = b;
+        this.C = c;
+    }
+
+}
